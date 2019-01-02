4be5405c2f53c0f6c4924d1ff9d3df2cb24fba1a
Cesar Ramirez <carr.gdl1@gmail.com>
2019-01-02 10:30:40 -0600

    Initial Commit

---
+++ Examples/CreationalPatterns/singletonExample.js
@@ -0,0 +1,24 @@
+let instance = null;
+
+class Car {
+    constructor(doors, engine, color) {
+        if (!instance) {
+            this.doors = doors;
+            this.engine = engine;
+            this.color = color;
+            instance = this;
+        } else {
+            return instance;
+        }
+    }
+}
+
+const civic = new Car(4,'v6', 'grey');
+
+//No matter what values are passed t othe new instance this values will not be
+//passed
+const honda = new Car(4,'v4', 'red');
+
+
+console.log(civic);
+console.log(honda);
---
+++ index.js
@@ -0,0 +1,24 @@
+class Car {
+    constructor(doors, engine, color) {
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
+}
+
+class carFactory {
+    createCar(type) {
+        switch(type) {
+            case 'civic':
+                return new Car(5, 'v6', 'grey');
+            case 'honda':
+                return new Car(6, 'v4', 'red');
+        }
+    }
+}
+
+const factory = new carFactory();
+const honda = factory.createCar('honda');
+console.log(honda);
+
+
